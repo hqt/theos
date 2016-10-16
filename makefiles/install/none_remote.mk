e765cd45ec12817a8fa54bb63148fca319f6a817
Huynh Quang Thao <huynhquangthao@gmail.com>
2016-10-16 19:53:14 +0700

    change ignore files

--- makefiles/install/none_remote.mk
+++ makefiles/install/none_remote.mk
@@ -1,1 +1,6 @@
-none_local.mk
+internal-install:: stage
+	install.mergeDir "$(THEOS_STAGING_DIR)" "/"
+
+internal-uninstall::
+	@echo "$(MAKE) uninstall is not supported when packaging is disabled" >&2
+	@exit 1
